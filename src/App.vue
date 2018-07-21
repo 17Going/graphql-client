f5242b3c11c33bdbf22a86463efc8c71135c07e7
zdluoa <17701023509@163.com>
2018-07-21 17:27:51 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApolloExample/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApolloExample from './components/ApolloExample.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ApolloExample
   }
 }
 </script>
---
+++ src/components/ApolloExample.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="apollo-example">
+    <button>登录</button>
+  </div>
+</template>
+
+<script>
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:7002/graphql'
+});
+
+client.query({
+  query: gql`
+    query user{
+      name
+    }
+  `,
+})
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+export default {
+  data () {
+    return {
+      id: 3
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    
+  },
+}
+</script>
+
+<style scoped>
+.form,
+.input,
+.apollo,
+.message {
+  padding: 12px;
+}
+
+.input {
+  font-family: inherit;
+  font-size: inherit;
+  border: solid 2px #ccc;
+  border-radius: 3px;
+}
+
+.error {
+  color: red;
+}
+</style>
